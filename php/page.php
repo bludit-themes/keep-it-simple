7a72922684fa8db8ec2401c9daa615ddfff31b08
Edi <egoetschel@clickwork.ch>
2018-08-14 18:54:26 +0200

    Update page.php

--- php/page.php
+++ php/page.php
@@ -1,116 +1,119 @@
 <!-- Post -->
 
-  <!-- Load Bludit Plugins: Page Begin -->
-	<?php Theme::plugins('pageBegin'); ?>
+<!-- Load Bludit Plugins: Page Begin -->
+<?php Theme::plugins('pageBegin'); ?>
 
-    <article class="entry">
+	<article class="entry">
 
-		  <header class="entry-header">
+		<header class="entry-header">
 
 			<h2 class="entry-title">
-			<?php echo $page->title(); ?>
+				<?php echo $page->title(); ?>
 			</h2>
-      
+				
 			<?php
-			if ($page->status() == "published") {
+			if ($page->status() == "published") {				
 
-      // Get the user who created the post.
-			$User = $page->user();
+				// Get the user who created the post.
+				$User = $page->user();
 
-			// Default author is the username.
-			$author = $User->username();
+				// Default author is the username.
+				$author = $User->username();
 
-			// If the user complete the first name or last name this will be the author.
-			if( Text::isNotEmpty($User->firstName()) || Text::isNotEmpty($User->lastName()) ) {
-			  $author = $User->firstName().' '.$User->lastName();
-			}
-            
-      echo '<div class="entry-meta">';
-      echo '<ul>';
-			echo '<li>'.$page->date().'</li>';
-			echo ' <span class="meta-sep">&bull;</span> ';
-
-			if ($page->category() ==! "") {
-			  echo '<li><a href="'.HTML_PATH_ROOT.$Url->filters('category').'/'.strtolower($page->category()).'">'.$page->category().'</a></li>';
+				// If the user complete the first name or last name this will be the author.
+				if( Text::isNotEmpty($User->firstName()) || Text::isNotEmpty($User->lastName()) ) {
+					$author = $User->firstName().' '.$User->lastName();
+				}
+		
+				echo <div class="entry-meta">
+				echo '<ul>';
+				echo '<li>'.$page->date().'</li>';
 				echo ' <span class="meta-sep">&bull;</span> ';
-			}
 
-      echo '<li>'.$author.'</li>';
-			echo '</ul>';
+				if ($page->category() ==! "") {
+					echo '<li><a href="'.HTML_PATH_ROOT.$Url->filters('category').'/'.strtolower($page->category()).'">'.$page->category().'</a></li>';
+					echo ' <span class="meta-sep">&bull;</span> ';
+				}
 
-			}
+				echo '<li>'.$author.'</li>';
+				echo '</ul>';
 
-			else {
-			  echo '<div class="meta-empty"></div>';
-			}
+				}
 
+				else {
+					echo '<div class="meta-empty"></div>';
+				}
+
+			}	
 			?>
 			</div>
 
-      </header>
+		</header>
 
 	    <!-- Cover image -->
-	    <?php if ($page->coverImage()): ?>		
-          <div class="entry-content-media">
-				    <div class="post-thumb">
-						  <img src="<?php echo $page->coverImage(); ?>"/>
-						</div>
-					</div>
-			<?php endif ?>
-
-			<div class="entry-content">
-			<?php echo $page->content(); ?>
+	    <?php if ($page->coverImage()): ?>
+		<div class="entry-content-media">
+			<div class="post-thumb">
+				<img src="<?php echo $page->coverImage(); ?>"/>
 			</div>
+		</div>
+		<?php endif ?>
 
-      <?php
-  		
-      if ($page->status() == "published") {
- 			  $tags = $Page->tags(true);
+		<div class="entry-content">
+		<?php echo $page->content(); ?>
+		</div>
 
-				$i = 0;
-				$len = count($tags);
+		<?php
+  		if ($page->status() == "published") {
 
-				if ( $len >= 1) {
-  			  echo '<p class="tags">';
+ 		$tags = $Page->tags(true);
 
-					if ($i == 0 &&  $len == 1 ) {
+		$i = 0;
+		$len = count($tags);
 
-					echo $L->get('Tag');
+		if ( $len >= 1) {
 
-					foreach($tags as $tagKey=>$tagName) {
-					  echo ': <a href="'.HTML_PATH_ROOT.$Url->filters('tag').'/'.$tagKey.'">'.$tagName.'</a>';
+			echo '<p class="tags">';
+
+			if ($i == 0 &&  $len == 1 ) {
+
+				echo $L->get('Tag');
+
+				foreach($tags as $tagKey=>$tagName) {
+					echo ': <a href="'.HTML_PATH_ROOT.$Url->filters('tag').'/'.$tagKey.'">'.$tagName.'</a>';
+				}
+			}
+
+			else {
+
+				echo $L->get('Tags');
+
+				foreach($tags as $tagKey=>$tagName) {
+
+					if ($i == 0) {
+						echo ': <a href="'.HTML_PATH_ROOT.$Url->filters('tag').'/'.$tagKey.'">'.$tagName.'</a>';
 					}
-        }
 
-				else {
+					else
+					{
+						echo ', <a href="'.HTML_PATH_ROOT.$Url->filters('tag').'/'.$tagKey.'">'.$tagName.'</a></li>';
+					}
 
-				  echo $L->get('Tags');
+					$i++;
 
-				  foreach($tags as $tagKey=>$tagName) {
+				}
 
-				    if ($i == 0) {
-				      echo ': <a href="'.HTML_PATH_ROOT.$Url->filters('tag').'/'.$tagKey.'">'.$tagName.'</a>';
-				      }
-				      else
-				      {
-				      echo ', <a href="'.HTML_PATH_ROOT.$Url->filters('tag').'/'.$tagKey.'">'.$tagName.'</a></li>';
-				      }
+			}
 
-				    $i++;
+			echo '</p>';
 
-			      }
+		}
 
-          }
+	}
 
-        echo '</p>';
+	?>
 
-        }
+	</article>
 
-      }
-
-      ?>
-
-    </article>
-
-	<!-- Load Bludit Plugins: Page End -->
-	<?php Theme::plugins('pageEnd'); ?>
+<!-- Load Bludit Plugins: Page End -->
+<?php Theme::plugins('pageEnd'); ?>
